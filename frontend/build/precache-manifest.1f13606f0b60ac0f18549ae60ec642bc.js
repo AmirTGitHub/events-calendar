120e35c49031ef3851ad1be99d2d768dae1c95b0
Amir <amir.gevari@gmail.com>
2019-02-25 23:11:07 +0000

    build locally and added to the git

---
+++ frontend/build/precache-manifest.1f13606f0b60ac0f18549ae60ec642bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91ebc0dfabc337e63b11",
+    "url": "/static/css/main.96b7e3ad.chunk.css"
+  },
+  {
+    "revision": "91ebc0dfabc337e63b11",
+    "url": "/static/js/main.91ebc0df.chunk.js"
+  },
+  {
+    "revision": "aa5398833f68967356b3",
+    "url": "/static/js/1.aa539883.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ef74ed1b9e10360fcf958ebbe54b1f7d",
+    "url": "/index.html"
+  }
+];
